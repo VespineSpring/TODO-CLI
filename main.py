e5fafc670134c6b33c349b4b3610c66221d8d251
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-09-07 15:16:02 +0530

    Started using rich and learnt table

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import typer
 from datetime import datetime
 
+from rich import print
+from rich.console import Console
+from rich.table import Table
+
+data = {
+    "name": "Rick",
+    "age": 42,
+    "items": [{"name": "Portal Gun"}, {"name": "Plumbus"}],
+    "active": True,
+    "affiliation": None,
+}
+
 app = typer.Typer()
 now = datetime.now()
+console = Console()
 
 
 @app.command()
 def hello(name: str) -> None:
-    print(f"Hello, {name}!")
+    print(f"[bold red]Hello[/bold red], [blue]{name}[/blue]!")
+    print(data)
 
 
 @app.command()
@@ -34,6 +48,14 @@ def date() -> None:
 def date_time() -> None:
     date_time: str = now.strftime("%A, %d %B, %Y at %I:%M %p")
     print(date_time)
+
+
+@app.command()
+def table():
+    table = Table("Name", "Item")
+    table.add_row("Rick", "Portal Gun")
+    table.add_row("Morty", "Plumbus")
+    console.print(table)
 
 
 if __name__ == "__main__":
