00a55b729380cf477cf759081cc32ee47c89279a
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-09-07 15:39:15 +0530

    erm actually started working on the todo app ;)

--- main.py
+++ main.py
@@ -1,60 +1,45 @@
 import typer
-from datetime import datetime
+import os
+import json
 
 from rich import print
 from rich.console import Console
 from rich.table import Table
 
-data = {
-    "name": "Rick",
-    "age": 42,
-    "items": [{"name": "Portal Gun"}, {"name": "Plumbus"}],
-    "active": True,
-    "affiliation": None,
-}
-
 app = typer.Typer()
-now = datetime.now()
+TASK_FILE = "tasks.json"
 console = Console()
 
 
-@app.command()
-def hello(name: str) -> None:
-    print(f"[bold red]Hello[/bold red], [blue]{name}[/blue]!")
-    print(data)
+if not os.path.exists(TASK_FILE) or os.path.getsize(TASK_FILE) == 0:
+    with open(TASK_FILE, "w") as file:
+        json.dump([], file)
 
 
 @app.command()
-def goodbye(name: str, formal: bool = False) -> None:
-    if formal:
-        print(f"Goodbye {name}. Have a nice day.")
-    else:
-        print(f"Bye {name}!")
+def add(task: str) -> None:
+    with open(TASK_FILE, "r") as file:
+        data = json.load(file)
+
+    data.append(task)
+    
+    with open(TASK_FILE, "w") as file:
+        json.dump(data, file, indent=4)
+
+    print("[bold red]Task has been added.[/bold red]")
+    print(f"[yellow]TASK:[/yellow] [blue]{task}[/blue]")
 
 
 @app.command()
-def time() -> None:
-    time: str = now.strftime("%I:%M %p")
-    print(time)
+def tasks() -> None:
+    table = Table("Tasks")
 
+    with open(TASK_FILE, "r") as file:
+        data = json.load(file)
 
-@app.command()
-def date() -> None:
-    date: str = now.strftime("%A, %d %B, %Y")
-    print(date)
+    for task in data:
+        table.add_row(task)
 
-
-@app.command()
-def date_time() -> None:
-    date_time: str = now.strftime("%A, %d %B, %Y at %I:%M %p")
-    print(date_time)
-
-
-@app.command()
-def table():
-    table = Table("Name", "Item")
-    table.add_row("Rick", "Portal Gun")
-    table.add_row("Morty", "Plumbus")
     console.print(table)
 
 
