0c940c4a8efd6328b39dec7eb5d9bd7031b89fdb
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-09-07 11:12:58 +0530

    Added datetime commands

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import typer
+from datetime import datetime
 
 app = typer.Typer()
+now = datetime.now()
 
 
 @app.command()
@@ -14,6 +16,24 @@ def goodbye(name: str, formal: bool = False) -> None:
         print(f"Goodbye {name}. Have a nice day.")
     else:
         print(f"Bye {name}!")
+
+
+@app.command()
+def time() -> None:
+    time: str = now.strftime("%I:%M %p")
+    print(time)
+
+
+@app.command()
+def date() -> None:
+    date: str = now.strftime("%A, %d %B, %Y")
+    print(date)
+
+
+@app.command()
+def date_time() -> None:
+    date_time: str = now.strftime("%A, %d %B, %Y at %I:%M %p")
+    print(date_time)
 
 
 if __name__ == "__main__":
